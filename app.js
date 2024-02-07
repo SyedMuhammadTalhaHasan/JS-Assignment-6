0797e22eeea5b7affe33b13858b41cef6e95c8a3
SyedMuhammadTalhaHasan <155646073+SyedMuhammadTalhaHasan@users.noreply.github.com>
2024-02-07 10:23:14 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+btn.addEventListener("click",() => alert("Button Clicked!"));
+
+divv.addEventListener("mouseover",() => divv.style = "background-color:black");
+divv.addEventListener("mouseout",() => divv.style = "");
